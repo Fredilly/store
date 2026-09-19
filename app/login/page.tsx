e606169a609fa17113b8d5b401df797d49af4992
Fred E <fredilly@article6.org>
2026-09-19 23:20:43 +0800

    fix: type auth capability response

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -16,7 +16,7 @@ export default function LoginPage() {
   useEffect(() => {
     fetch("/api/auth-capabilities", { cache: "no-store" })
       .then((response) => response.json())
-      .then((data) => setGoogleEnabled(Boolean(data.google)))
+      .then((data: { google?: boolean }) => setGoogleEnabled(Boolean(data.google)))
       .catch(() => setGoogleEnabled(false));
   }, []);
 
