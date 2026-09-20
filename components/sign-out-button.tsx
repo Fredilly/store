428e525538bcce94eb2b5fa14fc7e1dfc4d38da2
Fred E <fredilly@article6.org>
2026-09-20 22:43:48 +0800

    fix: make sign out work without client hydration

--- components/sign-out-button.tsx
+++ components/sign-out-button.tsx
@@ -1,18 +1,9 @@
-"use client";
-
-import { authClient } from "../lib/auth-client";
-
 export function SignOutButton() {
   return (
-    <button
-      className="textButton"
-      type="button"
-      onClick={async () => {
-        await authClient.signOut();
-        window.location.href = "/login";
-      }}
-    >
-      Sign out
-    </button>
+    <form action="/api/logout" method="post" className="signOutForm">
+      <button className="textButton" type="submit">
+        Sign out
+      </button>
+    </form>
   );
 }
